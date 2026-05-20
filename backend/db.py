6338a99b20b37126889d94d27f758e1f94d9df1a
bisma maheen <bisma.slhry@gmail.com>
2026-05-20 06:06:30 +0500

    Add PostgreSQL connection helpers

---
+++ backend/db.py
@@ -0,0 +1,49 @@
+# create DB connection
+
+import os
+import psycopg2
+from psycopg2 import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+        cursor_factory=RealDictCursor
+    )
+
+def fetch_all(query, params=None):
+
+    # call DB connection method
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # execute query
+    cursor.execute(query, params or ())
+    rows = cursor.fetchall()
+
+    # after executing query -> close DB connection
+    cursor.close()
+    conn.close()
+
+    return rows
+
+def fetch_one(query, params=None):
+    # call DB connection method
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # execute query
+    cursor.execute(query, params or ())
+    row = cursor.fetchone()
+
+    # after executing query -> close DB connection
+    cursor.close()
+    conn.close()
+
+    return row
